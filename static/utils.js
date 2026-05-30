5394323dfa3e1755f085cb45db0304df9f37f1fe
agubelu <agustinbd3@gmail.com>
2026-05-30 12:42:08 +0200

    Rework sync method

--- static/utils.js
+++ static/utils.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { syncUI } from './app.js';
+
 async function createAudioElement(url, container) {
     const cacheBuster = Math.random().toString(36).substring(2, 15);
     const urlNoCache = `${url}?nocache=${cacheBuster}`;
@@ -11,6 +13,16 @@ async function createAudioElement(url, container) {
     audio.controls = true;
     audio.autoplay = true;
     audio.src = urlNoCache;
+
+    // This allows the sync process and periodic ping to happen even when in the background on mobile devices
+    audio.addEventListener('timeupdate', () => {
+        // Throttle manually since timeupdate fires frequently
+        const now = Date.now();
+        if (!audio._lastPing || now - audio._lastPing > 15_000) {
+            audio._lastPing = now;
+            syncUI();
+        }
+    });
 
     container.innerHTML = '';
     container.appendChild(audio);
